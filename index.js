46c765d79ca18ded499529367a015df6fc144a10
Shubham Maurya <smmaurya29012001@gmail.com>
2022-08-21 09:30:44 -0700

    Adding Some Function and Attaching _ to Window

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ import _ from "./matlib.js";
 
 let arrayExample = [1, 2, 3, 4, NaN, false, 0, 100, -9];
 
-var a = _.findlast({ 1: 1, 2: 2, 3: 3, 4: undefined }, function (n) {
-  return n % 2 == 9;
-});
+window._ == _ ? (window._matlib = _) : (window._ = _);
 
-console.log(a);
+var users = [
+  { user: "barney", active: true },
+  { user: "fred", active: false },
+];
+
+// The `_.matchesProperty` iteratee shorthand.
+console.log(_.some(users, ["active", false]));
+// => true
