8771f958d6f304f17a841b07c577e8d93b94876b
Shubham Maurya <smmaurya29012001@gmail.com>
2022-08-22 09:41:15 -0700

    Adding Merge Function

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ import _ from "./matlib.js";
 let arrayExample = [1, 2, 3, 4, NaN, false, 0, 100, -9];
 
 window._ = _;
+var object = {
+  a: [{ b: 2 }, { d: 4 }],
+};
 
-var users = [
-  { user: "barney", age: 36, active: false },
-  { user: "fred", age: 40, active: true },
-  { user: "pebbles", age: 1, active: false },
-];
+var other = {
+  a: [{ c: 3 }, { e: 5 }],
+};
 
-console.log(_.groupby(["one", "two", "three"], "length"));
+console.log(_.merge(object, other));
